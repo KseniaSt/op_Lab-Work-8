7038b7d67c870867a73f126495184a3584bd0cec
KseniaSt <ksenia.stels@gmail.com>
2016-05-06 22:45:49 +0300

    Edit

--- opi.lab8/opi.lab8/functions.cpp
+++ opi.lab8/opi.lab8/functions.cpp
@@ -35,3 +35,22 @@ void output(char*name)
 	printf("\n");
 	fclose(file);
 }
+void edit()
+{
+	char s[N];
+	FILE *in = fopen("in.txt", "r");
+	FILE *out = fopen("out.txt", "w+");
+	if (in == NULL || out == NULL)
+	{
+		perror("This file can not be opened");
+		system("pause");
+	}
+	while (!feof(in))
+	{
+		fgets(s, sizeof(s), in);
+		if (s[strlen(s) - 2] != '.' && s[strlen(s) - 2] != ',' && s[strlen(s) - 2] != ' ') fprintf(out, "%c\n", '-');
+		else fprintf(out, "%s", s);
+	}
+	fclose(in);
+	fclose(out);
+}
